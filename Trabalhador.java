1538ae2a5d4b2251ba375847b552b42adae14a90
Talesrib <talespiano@gmail.com>
2024-10-03 12:51:57 -0300

    add classe trabalhador que processa pedido

---
+++ Trabalhador.java
@@ -0,0 +1,31 @@
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Trabalhador {
+  private ConcurrentHashMap<String, Integer> produtos;
+
+  public Trabalhador(ConcurrentHashMap<String, Integer> p) {
+    this.produtos = p;
+  }
+
+  public void processarPedido(Pedido pedido) throws Exception {
+    
+    for(int i = 0; i < pedido.produtos.size(); i++) {
+      String produto = pedido.produtos.get(i);
+      Integer quantity = pedido.quantity.get(i);
+
+      if (this.produtos.contains(produto) && this.produtos.get(produto) >= quantity) {
+        
+      } else {
+        throw new Exception("Pedido rejeitado!");
+      }
+    }
+
+    for(int i = 0; i < pedido.produtos.size(); i++) {
+      String produto = pedido.produtos.get(i);
+      Integer quantity = pedido.quantity.get(i);
+
+      this.produtos.put(produto, produtos.get(produto) - quantity);
+    }
+    System.out.println(String.format("Pedido de id %d processado", pedido.id));
+  }
+}
