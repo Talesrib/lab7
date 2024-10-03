8c8bf8f01f6e65c3d032ad88b476b771d6faa166
Talesrib <talespiano@gmail.com>
2024-10-03 12:47:46 -0300

    add classe pedido

---
+++ Pedido.java
@@ -0,0 +1,22 @@
+import java.util.List;
+import java.util.Random;
+
+public class Pedido {
+  List<String> produtos;
+  List<Integer> quantity;
+  Long id;
+
+  public Pedido(List<String> produtos, List<Integer> q) {
+    this.produtos = produtos;
+    this.quantity = q;
+    this.id = new Random().nextLong();
+  }
+
+  public String toString() {
+    String result = String.format("id: %d", id);
+    for (int e = 0; e < this.produtos.size(); e++) {
+      result += String.format("%s: %d", produtos.get(e), quantity.get(e));
+    }
+    return result;
+  }
+}
